3190875ab89047d9d18bd6dcba654afc4883a42d
MOHD SOHAIL <mdsohail9480@gmail.com>
2026-06-25 09:16:35 +0530

    updated website

---
+++ Javascript/Daily/Day 2/script.js
@@ -0,0 +1,86 @@
+// let day = parseInt(prompt("enter your number"))
+// let dayName;
+
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break
+//     case 2:
+//         console.log("tuesday");
+//         break
+//     case 3:
+//         console.log("wednesday");
+//         break
+//     case 5:
+//         console.log("thursday");
+//         break
+//     case 6:
+//         console.log("saturday");
+//         break
+//     case 7:
+//         console.log("sunday");
+//         break
+//     default:
+//         console.log("the number is out of range")
+//         break
+
+// }
+
+// let marks = parseInt(prompt("enter your marks"))
+// switch(true){
+//     case (marks>=41 && marks<=50):
+//         console.log("grade A")
+//         break
+//     case (marks >= 31 && marks >= 40):
+//         console.log("grade B")
+//         break
+//     case (marks>=21 && marks >= 30):
+//         console.log("grade C")
+//         break
+//     case (marks >=11 && marks >=20):
+//         console.log("grade D")
+//         break
+//     case (marks >=0 && marks >=10):
+//         console.log("grade F")
+// }
+
+// let str = "we are developer "
+
+// let res = str.indexOf("Developer")
+
+// console.log(res);
+
+
+// let obj={
+//     name:"sohail",
+//     age : 21,
+//     email: "mdsohai@gmail.com "
+// }
+// for(let key in obj){
+//     console.log(`${key}=${obj[key]}`)
+// }
+
+// var name = "you will  an amazing developer"
+// var newMessage = name.slice(7,13);
+// console.log(newMessage)
+// newMessage = name.slice(-7,-13);
+// console.log(newMessage)
+// newMessage = name.slice(7);
+// console.log(newMessage)
+// newMessage = name.slice(-7)
+// console.log(newMessage)
+
+// Replace
+
+var name = "You will be amazind developer "
+// name.replace('','_');
+// console.log(name)
+// name.replaceAll('','_')
+// console.log(name)
+  
+
+// Split 
+
+// var nameList = name.split(" ");
+
+console.log(name.length);
---
+++ Javascript/Daily/Day 3/script.js
@@ -0,0 +1,38 @@
+// ARRAY day 3 topic 
+
+// let arr = ['hello','sohail','kya hal ahai']
+// console.log(arr);
+
+// PUSH method 
+// push method add new elemet at the end of the arry and return the new length 
+
+// const arr = ['java','the'];
+// arr.push("Developer")
+// console.log(arr)
+
+// SLICE method 
+// select a part of the arry and return the new arry 
+
+// const arr = ['java','the','developer'];
+// console.log(arr.slice(1,2))
+
+// SHIFT method 
+
+// const arr = ['java','the','developer']
+// console.log(arr.shift());
+
+// MAP function 
+// the Map() method creates a new arry with with the result of calling a function for every arry element 
+
+// const arr = [1,2,3,54];
+// console.log(arr.map())
+
+function func() {
+    console.log("Regular function")
+}
+
+function fun (name,age){
+    return console.log("Reguler Function", name, age);
+}
+
+fun("aman", 22)
